ddc113f5509c7803c5f48d835ad21d84f5370b19
rohan19a <46227014+rohan19a@users.noreply.github.com>
2023-07-09 00:04:26 -0700

    Add files via upload

---
+++ BerkeleySpots/HW72/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  HW72
+//
+//  Created by Abinaya on 3/21/23.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseCore
+
+struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+    //TODO: add a variable that tracks whether the user has successfully logged in or not
+    @State var loggedIn = false
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            } else {
+//                TODO: update the variable to track that the user has successfully logged in
+                loggedIn = true
+                print("success")
+            }
+        }
+    }
+
+    var body: some View {
+        //TODO: Implement an if-else to return a new view when a certain condition is met
+        if loggedIn {
+            MainView()
+        } else {
+            ContentView()
+        }
+        TextField("Email", text: $email).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
+            TextField("Password", text: $password).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
+                
+            Button(action: { login() }){
+                     Text("Sign in")
+                 }.buttonStyle(. bordered).tint(.mint)
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BerkeleySpots/HW72/HW72App.swift
@@ -0,0 +1,32 @@
+//
+//  HW72App.swift
+//  HW72
+//
+//  Created by Abinaya on 3/21/23.
+//
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct HW72App: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
